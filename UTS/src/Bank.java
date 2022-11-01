dba506aa6f6f5913ab9e832c7a416bebc1ca1d2a
Alkarizqy Restu Pribadi Budianto <115544078+AlkarizqyRPB@users.noreply.github.com>
2022-11-01 16:59:50 +0700

    Add files via upload

---
+++ UTS/src/Bank.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alkarizqy
+ */
+public class Bank {
+    int saldo = 100000;
+    
+   public void ambilDuit(int b){
+       saldo = saldo - b;
+       if (saldo <= saldo ){
+        System.out.println("Saldo Tidak Mencukupi ");
+       } else{
+        System.out.println("Saldo saat ini : " + saldo);
+       }
+   }
+   public void simpanDuit(int b){
+       saldo = saldo + b;
+     System.out.println("Saldo saat ini : " + saldo);      
+   }
+    public void getSaldo(){
+    System.out.println("Saldo saat ini : " + saldo);
+    
+   }  
+}
